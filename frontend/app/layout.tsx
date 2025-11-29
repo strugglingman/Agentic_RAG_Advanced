baff8b8c7fe5754fc295104b7bce9f1d129bfccf
xinwei <xinwei.wang@atex.com>
2025-11-30 00:52:23 +0100

    started theme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,10 +1,13 @@
 import "./globals.css";
+import RootThemeProvider from "@/components/RootThemeProvider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="h-screen overflow-hidden bg-neutral-100">
-        {children}
+      <body className="h-screen overflow-hidden bg-neutral-100 dark:bg-neutral-900">
+        <RootThemeProvider>
+          {children}
+        </RootThemeProvider>
       </body>
     </html>
   );
