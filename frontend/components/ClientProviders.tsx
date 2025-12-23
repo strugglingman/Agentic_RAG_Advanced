dce4620804682ccccc66ebbc9d48f1ea18ef54e1
xinwei <strugglingman@gmail.com>
2025-12-23 09:35:29 +0100

    react markdown reference

--- frontend/components/ClientProviders.tsx
+++ frontend/components/ClientProviders.tsx
@@ -1,15 +1,18 @@
 "use client";
 import { FiltersProvider } from "@/components/filters-context";
 import { ChatProvider } from "@/components/chat-context";
+import { ThemeProvider } from "@/components/theme-context";
 import { SessionProvider } from "next-auth/react";
 export default function ClientProviders({ children, session }: { children: React.ReactNode, session: any }) {
   return (
     <SessionProvider session={session}>
-      <FiltersProvider>
-        <ChatProvider>  
-          {children}
-        </ChatProvider>
-      </FiltersProvider>
+      <ThemeProvider>
+        <FiltersProvider>
+          <ChatProvider>
+            {children}
+          </ChatProvider>
+        </FiltersProvider>
+      </ThemeProvider>
     </SessionProvider>
   );
 }
