030bd47a5d4d9c61b7cde1c71971afea2c8cda16
xinwei <strugglingman@gmail.com>
2026-02-17 01:09:29 +0100

    fix int convertion crash

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -434,7 +434,7 @@ class Config:
     # Email Settings
     # ==============================================================================
     SMTP_SERVER = os.getenv("SMTP_SERVER", "")
-    SMTP_PORT = int(os.getenv("SMTP_PORT", ""))
+    SMTP_PORT = int(os.getenv("SMTP_PORT") or "587")
     SMTP_USER = os.getenv("SMTP_USER", "")
     SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", "")
 
